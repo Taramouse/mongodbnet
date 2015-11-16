efd69cadcb26141ca02ea6804b046cdc9b4cf9af
Taramouse <taramouse@live.co.uk>
2015-11-16 19:48:58 +0000

    Added MongoDB. Used NuGet to install MongoDB .Net driver and dependancies. Added database context and connected to 'test' database. Then printed out the 'testData' collection data to the console.

--- MongoDB.Net/MongoDB.Net/Program.cs
+++ MongoDB.Net/MongoDB.Net/Program.cs
@@ -1,15 +1,41 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using MongoDB.Driver;
+using MongoDB.Bson;
 
 namespace MongoDB.Net
 {
     class Program
     {
+        private static MongoClient client;
+        private static IMongoCollection<BsonDocument> collection;
+        private static IMongoDatabase database;
+
         static void Main(string[] args)
         {
+            // Connect to the server.
+            client = new MongoClient("mongodb://localhost:27017");
+            Console.WriteLine("Connecting to localhost:27017... ");
+            Console.WriteLine("Connected.");
+
+            // Connect to test database.
+            database = client.GetDatabase("test");
+            Console.WriteLine("Connected to 'test' database");
+
+            // Get the database collection.
+            getCollection();
+            
+            // Make console wait for user to close.
+            Console.WriteLine("Press 'CTRL + C' to exit...");
+            Console.Read();
+        }
+
+        static async void getCollection()
+        {
+            collection = database.GetCollection<BsonDocument>("testData");
+            // Find all documents in collection and convert to a List
+            Console.WriteLine("testData collection list:");
+            await collection.Find(new BsonDocument()).ForEachAsync(d => Console.WriteLine(d));
+      
         }
     }
 }
